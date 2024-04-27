4cd54a776160b8b8bb760a2e81c530ad23dfc4a0
ivan <rineroriner@gmail.com>
2024-04-27 07:03:34 +0300

    тестирую без презентера,щупаю фильтры

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 import {render} from './render.js';
+import NewTaskHeaderFilterView from './view/new-task-header-filter-view.js';
+
+const siteHeaderFilterElement = document.querySelector('.trip-controls__filters');
+
+render(new NewTaskHeaderFilterView(), siteHeaderFilterElement);
+
+
