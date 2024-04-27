c7294a69388d3db264883ba7aee3deaf7a8e1f22
ivan <rineroriner@gmail.com>
2024-04-27 08:28:21 +0300

    fix conflicts Co-authored-by: baileys-li <baileys-li@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 import {render} from './render.js';
 import NewTaskHeaderFilterView from './view/new-task-header-filter-view.js';
 
 const siteHeaderFilterElement = document.querySelector('.trip-controls__filters');
 
 render(new NewTaskHeaderFilterView(), siteHeaderFilterElement);
-
-=======
->>>>>>> aed5dc947155b905c01408da95613952cd38761d
-
