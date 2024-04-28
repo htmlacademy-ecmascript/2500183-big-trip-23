e1c182eee5e6cc3f39f8423ede291d4699c5f640
ivan <rineroriner@gmail.com>
2024-04-28 06:44:57 +0300

    позиционирование в шапке, подобие презентера

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
-import {render} from './render.js';
+import {render,RenderPosition} from './render.js';
 import NewTripFiltersView from './view/trip-filters-view.js';
 import NewTripInfoHeader from './view/trip-info-header.js';
 import NewTripEventsSortView from './view/trip-events-sort-view.js';
 import NewTripEventsListView from './view/trip-events-list-view.js';
 
 const siteHeaderFilterElement = document.querySelector('.trip-controls__filters');
-const siteHeaderTripMainContainer = document.querySelector('.trip-main');// флекс headera
+const siteHeaderTripMainContainer = document.querySelector('.trip-main');
 const siteTripEventsSection = document.querySelector('.trip-events');
 
-render(new NewTripInfoHeader(), siteHeaderTripMainContainer);
-render(new NewTripFiltersView(), siteHeaderFilterElement);
-render(new NewTripEventsSortView(),siteTripEventsSection);
-render(new NewTripEventsListView(),siteTripEventsSection);
+function init() {
+  render(new NewTripInfoHeader(), siteHeaderTripMainContainer,RenderPosition.AFTERBEGIN);
+  render(new NewTripFiltersView(), siteHeaderFilterElement);
+  render(new NewTripEventsSortView(),siteTripEventsSection);
+  render(new NewTripEventsListView(),siteTripEventsSection);
+}
+
+init();
+
