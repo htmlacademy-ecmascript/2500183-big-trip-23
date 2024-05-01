5f0274399075aa37c9dc4ec91614bb7dd6ab6434
ivan <rineroriner@gmail.com>
2024-05-01 15:51:25 +0300

    подготовка для моковых данных

--- src/presenter/new-presenter.js
+++ src/presenter/new-presenter.js
@@ -1,10 +1,12 @@
 import {render,RenderPosition} from '../render';
-import NewTripEventsListView from '../view/trip-events-list-view';
 import NewTripEventsSortView from '../view/trip-events-sort-view';
 import NewTripInfoHeader from '../view/trip-info-header';
 import NewTripFiltersView from '../view/trip-filters-view';
+import NewTripEventsListView from '../view/trip-events-list-view';
+import NewTripEventsPointView from '../view/trip-events-points-view';
 
 export default class BoardPresenter {
+  containerListComponent = new NewTripEventsListView();
   constructor() {
     this.siteHeaderFilterElement = document.querySelector('.trip-controls__filters');
     this.siteHeaderTripMainContainer = document.querySelector('.trip-main');
@@ -15,6 +17,10 @@ export default class BoardPresenter {
     render(new NewTripFiltersView(), this.siteHeaderFilterElement);
     render(new NewTripInfoHeader(), this.siteHeaderTripMainContainer,RenderPosition.AFTERBEGIN);
     render(new NewTripEventsSortView(),this.siteTripEventsSection);
-    render(new NewTripEventsListView(),this.siteTripEventsSection);
+    render(this.containerListComponent,this.siteTripEventsSection);
+
+    for (let i = 0; i < 3; i++) {
+      render(new NewTripEventsPointView(),this.containerListComponent.getElement());
+    }
   }
 }
