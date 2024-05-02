6233d8fd82be1eb58256fb17e703616ea41c7cbe
ivan <rineroriner@gmail.com>
2024-05-03 00:52:36 +0300

    установил ts? вроде работает

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import BoardPresenter from './presenter/new-presenter.js';
 //import {generateAddWaypoint} from './mock/points.js';
 const myBoardepresenters = new BoardPresenter();
 
+import './test.ts';
+
 myBoardepresenters.init();
 
 //console.log(generateAddWaypoint());
