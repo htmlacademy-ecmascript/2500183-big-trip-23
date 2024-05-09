7496ea67b3b369a7db03649a3a330ee67635c5be
ivan <rineroriner@gmail.com>
2024-05-09 18:51:44 +0300

    первый тест передачи функций

--- src/presenter/main-presenter.js
+++ src/presenter/main-presenter.js
@@ -1,4 +1,4 @@
-import {render} from '../framework/render.js';
+import {render,replace} from '../framework/render.js';
 import NewTripEventsSortView from '../view/trip-events-sort-view';//
 import NewTripEventsListView from '../view/trip-events-list-view';//
 import NewTripEventsPointView from '../view/trip-events-points-view';//
@@ -21,18 +21,29 @@ export default class MainPresenter {
 
     const offersTest = this.#pointModel.offers;
 
+
     render(new NewTripEventsSortView(), this.#boardContainer);
     render(this.#containerListComponent, this.#boardContainer);
     render(new NewTripEventsAddPointView(), this.#containerListComponent.element);
-    render(new NewTripEventsEditPointView(points[0], destination,offersTest),this.#containerListComponent.element);
+
 
     points.forEach((point) => {
       this.#renderPoint(point,destination,offersTest);
     });
+
   }
 
   #renderPoint(point,destination,offersTest) {
-    const tripPointComponent = new NewTripEventsPointView(point,destination,offersTest);
+    const onEditClick = () => onEditClickTest();
+
+    const tripPointComponent = new NewTripEventsPointView(point,destination,offersTest,onEditClick);
+    const tripEditComponent = new NewTripEventsEditPointView(point, destination,offersTest);
+
+
+    function onEditClickTest() {
+      replace(tripEditComponent,tripPointComponent);
+    }
+
     render(tripPointComponent,this.#containerListComponent.element);
   }
 }
--- src/view/trip-events-points-view.js
+++ src/view/trip-events-points-view.js
@@ -60,15 +60,24 @@ export default class NewTripEventsPointView extends AbstractView {
   #point = null;
   #destination = null;
   #offers = null;
-  constructor(point,destination,offers){
+  #onEditClick = null;
+  #rollupButton = null;
+  constructor(point,destination,offers,onEditClick){
     super();
     this.#point = point;
     this.#destination = destination;
     this.#offers = offers;
-
+    this.#onEditClick = onEditClick;
+    this.#rollupButton = this.element.querySelector('.event__rollup-btn');
+    this.#rollupButton.addEventListener('click',this.#onClick);
   }
 
   get template() {
-    return createTripEventsPointElements(this.#point,this.#destination,this.#offers);
+    return createTripEventsPointElements(this.#point,this.#destination,this.#offers,this.#rollupButton);
   }
+
+  #onClick = (evt) => {
+    evt.preventDefault();
+    this.#onEditClick();
+  };
 }
