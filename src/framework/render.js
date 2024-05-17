4d20d864b5e71e0acf4450106cc49605ca9db787
ivan <rineroriner@gmail.com>
2024-05-17 18:26:51 +0300

    change lint

--- src/framework/render.js
+++ src/framework/render.js
@@ -32,7 +32,7 @@ function render(component, container, place = RenderPosition.BEFOREEND) {
   }
 
   if (container === null) {
-    throw new Error("Container element doesn't exist");
+    throw new Error('Container element doesn\'t exist');
   }
 
   container.insertAdjacentElement(place, component.element);
@@ -54,7 +54,7 @@ function replace(newComponent, oldComponent) {
   const parent = oldElement.parentElement;
 
   if (parent === null) {
-    throw new Error("Parent element doesn't exist");
+    throw new Error('Parent element doesn\'t exist');
   }
 
   parent.replaceChild(newElement, oldElement);
