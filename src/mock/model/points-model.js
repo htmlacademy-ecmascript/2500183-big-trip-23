8d29c8958bd7a2184907c2a7e8df5b09e1e1de3c
ivan <rineroriner@gmail.com>
2024-05-28 19:31:56 +0300

    добавил примерные методы для точек

--- src/mock/model/points-model.js
+++ src/mock/model/points-model.js
@@ -37,4 +37,44 @@ export default class PointModel extends Observable {
   getOffersByType(type) {
     return this.#offers.find((offer) => offer.type === type).offers;
   }
+
+  updatePoint(updateType, update) {
+    const index = this.#points.findIndex((task) => task.id === update.id);
+
+    if (index === -1) {
+      throw new Error('Can\'t update unexisting task');
+    }
+
+    this.#points = [
+      ...this.#points.slice(0, index),
+      update,
+      ...this.#points.slice(index + 1),
+    ];
+
+    this._notify(updateType, update);
+  }
+
+  addPoint(updateType, update) {
+    this.#points = [
+      update,
+      ...this.#points,
+    ];
+
+    this._notify(updateType, update);
+  }
+
+  deletePoint(updateType, update) {
+    const index = this.#points.findIndex((task) => task.id === update.id);
+
+    if (index === -1) {
+      throw new Error('Can\'t delete unexisting task');
+    }
+
+    this.#points = [
+      ...this.#points.slice(0, index),
+      ...this.#points.slice(index + 1),
+    ];
+
+    this._notify(updateType);
+  }
 }
