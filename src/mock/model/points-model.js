0c40aebe973a55a1e9f57d30c2dc709b5c86af09
ivan <rineroriner@gmail.com>
2024-06-09 18:28:43 +0300

    тестирую адаптер,вроде работает,как ни странно!

--- src/mock/model/points-model.js
+++ src/mock/model/points-model.js
@@ -19,7 +19,17 @@ export default class PointModel extends Observable {
 
     this.#pointsApiService = pointsApiService;
 
-    this.#pointsApiService.points.then((point) => console.log(point));
+    //this.#pointsApiService.points.then((point) => {
+     // console.log(point));
+    //}
+    this.#pointsApiService.points.then((points) => {
+      console.log(points);
+      // Есть проблема: cтруктура объекта похожа, но некоторые ключи называются иначе,
+      // а ещё на сервере используется snake_case, а у нас camelCase.
+      // Можно, конечно, переписать часть нашего клиентского приложения, но зачем?
+      // Есть вариант получше - паттерн "Адаптер"
+      console.log(points.map(this.#adaptToClient));
+    });
 
   }
 
@@ -88,5 +98,21 @@ export default class PointModel extends Observable {
     this.#points = [...this.#points.slice(0, index), ...this.#points.slice(index + 1)];
 
     this._notify(updateType);
+  }
+
+  #adaptToClient(point) {
+    const adaptedPoint = {...point,
+      basePrice: point['base_price'],
+      dateFrom: point['date_from'] !== null ? new Date(point['date_from']) : point['date_from'],
+      dateTo: point['date_to'] !== null ? new Date(point['date_to']) : point['date_to'],
+      isFavorite: point['is_favorite'],
+    };
+
+    delete adaptedPoint['base_price'];
+    delete adaptedPoint['date_from'];
+    delete adaptedPoint['date_to'];
+    delete adaptedPoint['is_favorite'];
+
+    return adaptedPoint;
   }
 }
