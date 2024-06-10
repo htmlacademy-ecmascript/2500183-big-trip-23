7e770991efbac9e9df42ec7b3f50b1e6c30b5a00
ivan <rineroriner@gmail.com>
2024-06-10 07:35:25 +0300

    сделал обновление задчаи! -8.1 закончил!

--- src/mock/model/points-model.js
+++ src/mock/model/points-model.js
@@ -65,16 +65,28 @@ export default class PointModel extends Observable {
     return this.#offers.find((offer) => offer.type === type).offers || [];
   }
 
-  updatePoint(updateType, update) {
+  async updatePoint(updateType, update) {
     const index = this.#points.findIndex((task) => task.id === update.id);
 
     if (index === -1) {
       throw new Error('Can\'t update unexisting point');
     }
 
-    this.#points = [...this.#points.slice(0, index), update, ...this.#points.slice(index + 1)];
+    //this.#points = [...this.#points.slice(0, index), update, ...this.#points.slice(index + 1)];
 
-    this._notify(updateType, update);
+    //this._notify(updateType, update);
+    try {
+      const response = await this.#pointsApiService.updatePoint(update);
+      const updatedPoint = this.#adaptToClient(response);
+      this.#points = [
+        ...this.#points.slice(0, index),
+        updatedPoint,
+        ...this.#points.slice(index + 1),
+      ];
+      this._notify(updateType, updatedPoint);
+    } catch(err) {
+      throw new Error('Can\'t update point');
+    }
   }
 
   addPoint(updateType, update) {
