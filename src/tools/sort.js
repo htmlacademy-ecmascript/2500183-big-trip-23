f583d64ba561fa9560409e52c01e8165b162612a
ivan <rineroriner@gmail.com>
2024-06-12 10:59:00 +0300

    поправил сортировку

--- src/tools/sort.js
+++ src/tools/sort.js
@@ -1,11 +1,14 @@
 import { SortType } from '../mock/const.js';
 
-const getTimeDifferens = ({ dateFrom, dateTo }) => new Date(dateTo).getTime() - new Date(dateFrom).getTime();
-
-const sortPointBy = {
-  [SortType.DAY]: (points) => points,
-  [SortType.TIME]: (points) => points.toSorted((a, b) => getTimeDifferens(b) - getTimeDifferens(a)),
-  [SortType.PRICE]: (points) => points.toSorted((a, b) => b.basePrice - a.basePrice),
+const sortBy = {
+  [SortType.DAY]: (points) => [...points].sort((firstPoint, secondPoint) => new Date(firstPoint.dateFrom) - new Date(secondPoint.dateFrom)),
+  [SortType.TIME]: (points) => [...points].sort((firstPoint, secondPoint) => {
+    const durationFirstPoint = new Date(firstPoint.dateTo).getTime() - new Date(firstPoint.dateFrom).getTime();
+    const durationSecondPoint = new Date(secondPoint.dateTo).getTime() - new Date(secondPoint.dateFrom).getTime();
+    return durationSecondPoint - durationFirstPoint;
+  }),
+  [SortType.PRICE]: (points) => [...points].sort((firstPoint, secondPoint) => secondPoint.basePrice - firstPoint.basePrice),
 };
 
-export const sortPoints = (points, sortType) => sortPointBy[sortType](points);
+export const sortPoints = (points, sortType) => sortBy[sortType](points);
+
